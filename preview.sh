0d2bbcfbfb1e970a495c7c9af6900c35e3dbb51b
Marcus Fihlon <marcus@fihlon.swiss>
2024-06-21 14:48:34 +0200

    🐛 Im Header dürfen keine Leerzeilen sein

--- preview.sh
+++ preview.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p preview
-asciidoctor -a toc --backend html5 --out-file index.html --section-numbers --destination-dir preview docs/index.adoc
+asciidoctor --backend html5 --out-file index.html --section-numbers --destination-dir preview docs/index.adoc
