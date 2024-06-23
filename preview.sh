c23d859e68adcd5731c45450bd18411e97d2f781
Marcus Fihlon <marcus@fihlon.swiss>
2024-06-23 10:22:36 +0200

    ♻️ Dokumentation aufgeteilt

--- preview.sh
+++ preview.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-mkdir -p preview
-asciidoctor --backend html5 --out-file index.html --section-numbers --destination-dir preview docs/index.adoc
+mkdir -p preview/user
+mkdir -p preview/technology
+cp docs/index.html preview/
+asciidoctor --backend html5 --out-file index.html --section-numbers --destination-dir preview/user docs/user/index.adoc
+asciidoctor --backend html5 --out-file index.html --section-numbers --destination-dir preview/technology docs/technology/index.adoc
